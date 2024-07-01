d7a79de2b4180ad462c77d4338a46c97091d660d
Pranay Narsipuram <pnarsipuram@login01.hpc.rd.areasciencepark.it>
2024-07-01 18:52:10 +0000

    updated the job script for weak scaling tests

--- HPC/exercise2/mpi_broadcast/mpi_openmp_mandelbrot/run_mandelbrot.sh
+++ HPC/exercise2/mpi_broadcast/mpi_openmp_mandelbrot/run_mandelbrot.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=strong_scaling_mandelbrot
-#SBATCH --output=strong_scaling_output.txt
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16  # Update this for each run (1, 2, 4, 8, 16)
+#SBATCH --job-name=weak_scaling_mandelbrot
+#SBATCH --output=weak_scaling_output.txt
+#SBATCH --ntasks=16           # Update this for each run (2, 4, 8, 16)
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=2            # Ensure using at least 2 nodes
 #SBATCH --time=00:10:00
 #SBATCH --partition=EPYC
 
@@ -10,5 +11,5 @@ module load openMPI/4.1.5/gnu/12.2.1
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-mpirun ./mpi_openmp_mandelbrot 1000 1000 -2.0 -2.0 2.0 2.0 1000
+mpirun ./mpi_openmp_mandelbrot 2000 2000 -2.0 -2.0 2.0 2.0 1000  # Increase problem size proportionally
 
