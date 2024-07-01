2ce5c2757f3dc457b085e12d488ce09d33e0c961
Pranay Narsipuram <pnarsipuram@login01.hpc.rd.areasciencepark.it>
2024-07-01 18:48:33 +0000

    updated the job script for strong scaling tests

--- HPC/exercise2/mpi_broadcast/mpi_openmp_mandelbrot/run_mandelbrot.sh
+++ HPC/exercise2/mpi_broadcast/mpi_openmp_mandelbrot/run_mandelbrot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=mpi_openmp_mandelbrot
-#SBATCH --output=mandelbrot_output.txt
-#SBATCH --ntasks=4           # Number of MPI tasks
-#SBATCH --cpus-per-task=4    # Number of OpenMP threads per MPI task
+#SBATCH --job-name=strong_scaling_mandelbrot
+#SBATCH --output=strong_scaling_output.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16  # Update this for each run (1, 2, 4, 8, 16)
 #SBATCH --time=00:10:00
 #SBATCH --partition=EPYC
 
@@ -11,3 +11,4 @@ module load openMPI/4.1.5/gnu/12.2.1
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 mpirun ./mpi_openmp_mandelbrot 1000 1000 -2.0 -2.0 2.0 2.0 1000
+
